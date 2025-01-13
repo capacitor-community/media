119edba3719e9afb4fba10662e77e48d4b2b9f22
Michael Podolsky <michaelspodolsky@gmail.com>
2025-01-13 11:07:54 -0800

    update getMediaByIdentifier compression level

--- ios/Plugin/MediaPlugin.swift
+++ ios/Plugin/MediaPlugin.swift
@@ -83,7 +83,7 @@ public class MediaPlugin: CAPPlugin {
                     var jpegData: Data
                            if ext != "jpg" && ext != "jpeg" {
                                if let image = UIImage(data: imageData),
-                                  let jpegDataConverted = image.jpegData(compressionQuality: 1.0) {
+                                  let jpegDataConverted = image.jpegData(compressionQuality: 0.8) {
                                    jpegData = jpegDataConverted
                                    ext = "jpg"
                                } else {
